a814b266f2ee42ae2c0d465ae8bc6244f8f1af69
Alice Lipski <alipski@pop-os.localdomain>
2022-12-20 16:35:09 -0300

    feat: create ticket at db with status reserverd

--- src/hooks/api/useTicket.js
+++ src/hooks/api/useTicket.js
@@ -2,14 +2,14 @@ import useAsync from '../useAsync';
 import useToken from '../useToken';
 import * as ticketApi from '../../services/ticketApi';
 
-export default function useTicket(body) {
+export default function useTicket() {
   const token = useToken();
   const {
     data: ticket,
     loading: ticketLoading,
     error: ticketError,
     act: postTicket
-  } = useAsync( () => ticketApi.createTicket(body, token));
+  } = useAsync( (body) => ticketApi.createTicket(body, token));
 
   return{
     ticket,
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -4,133 +4,159 @@ import usePaymentPaid from '../../../hooks/api/usePayment';
 import { useEffect, useState } from 'react';
 import ChoiceBox from '../../../components/Payment/ChoiceBox';
 import CreditCard from '../../../components/Payment/CreditCard';
+import useTicketType from '../../../hooks/api/useTicketType';
+import useTicket from '../../../hooks/api/useTicket';
 
 export default function Payment() {
   const { paymentLoading, payment, enrollmentLoading, enrollment } = usePaymentPaid();
-  const [ enrollmentData, setEnrollmentData] = useState('');
-  const [ paymentData, setPaymentData] = useState('');
-  const [ typeSelector, setTypeSelector ] = useState('');
-  const [ hotelSelector, setHotelSelector ] = useState('');
-  const [ typePrice, setTypePrice ] = useState(0);
-  const [ hotelPrice, setHotelPrice ] = useState(0);
-  const [ ticket, setTicket ] = useState({
+  const { getTicketType } = useTicketType();
+  const { postTicket } = useTicket();
+  const [enrollmentData, setEnrollmentData] = useState('');
+  const [paymentData, setPaymentData] = useState('');
+  const [typeSelector, setTypeSelector] = useState('');
+  const [hotelSelector, setHotelSelector] = useState('');
+  const [typePrice, setTypePrice] = useState(0);
+  const [hotelPrice, setHotelPrice] = useState(0);
+  const [ticket, setTicket] = useState({
     isRemote: false,
     includesHotel: false,
     price: 0,
   });
   const [reserve, setReserve] = useState(false);
   const ticketId = 3;
-  
+
   try {
     useEffect(() => {
-      if(payment) {
+      if (payment) {
         setPaymentData(payment);
       }
     }, [payment]);
 
     useEffect(() => {
-      if(enrollment) {
+      if (enrollment) {
         setEnrollmentData(enrollment);
       }
-    }, [enrollment]); 
-    
-    useEffect(() => {
-      console.log(ticket);
-    }, [ticket]); 
+    }, [enrollment]);
 
-    function submitTicket() {
-      setTicket({
-        isRemote: !typeSelector,
-        includesHotel: hotelSelector === '' || !typeSelector === true ? false : !hotelSelector,
-        price: typePrice === 100 ? Number(typePrice) : Number(typePrice) + Number(hotelPrice),
-      });
+    useEffect(async() => {
+      const ticketTypes = await getTicketType();
+      const type = ticketTypes.filter(
+        (item) => item.isRemote === ticket.isRemote && item.includesHotel === ticket.includesHotel
+      );
+      submitTicket(type);
+    }, [ticket]);
+
+    function submitTicket(type) {
+      if(type) {
+        const id = type[0].id;
+        postTicket({ id: id });
+      }
     }
-   
+
     return (
       <Wrapper>
         <h1>Ingresso e Pagamento</h1>
-        {paymentLoading || enrollmentLoading ? <span>
-          {<StyledLoader color="#000000" height={26} width={26} type="Oval" />} Carregando
-        </span> : enrollmentData !== '' ? <>
-          {paymentData !== '' && paymentData.status === 'PAID' ? <>
-            <h4>Ingresso escolhido</h4>
-            <Choices>
-              <ChoiceBox
-                description={(paymentData.TicketType.isRemote ? 'Remoto' : 'Presencial')+(paymentData.TicketType.includesHotel ? ' + Com Hotel' : ' + Sem Hotel')}
-                price={Number(paymentData.TicketType.price)}
-                selectState={true}
-                disable={true}
-              />
-            </Choices>
-            <h4>Pagamento</h4>
-            <>
-              <h3>Pagamento confirmado!</h3>
-              <h2>Prossiga para a escolha de hospedagem e atividades</h2>
-            </>
-          </> : paymentData !== '' && paymentData.status === 'RESERVED' ? 
-            <>
-              <Choices>
-                <ChoiceBox
-                  description= {'Presencial'+' + Com Hotel'}
-                  price={500}
-                  selectState={true}
-                  disable={true}
-                />
-              </Choices>
-              <CreditCard ticketId = {ticketId}/>
-            </> : <>
-              <h4>Primeiro, escolha sua modalidade de ingresso</h4>
-              <Choices>
-                <ChoiceBox
-                  description={'Presencial'}
-                  price={250}
-                  selectState={typeSelector === '' ? false : typeSelector}
-                  selector={setTypeSelector}
-                  setPrice={setTypePrice}
-                />
-                <ChoiceBox
-                  description={'Remoto'}
-                  price={100}
-                  selectState={typeSelector === '' ? false : !typeSelector}
-                  selector={setTypeSelector}
-                  setPrice={setTypePrice}
-                />
-              </Choices>
-              <SecondStep remote={!typeSelector}>
-                <h4>Ótimo! Agora escolha sua modalidade de hospedagem</h4>
+        {paymentLoading || enrollmentLoading ? (
+          <span>{<StyledLoader color="#000000" height={26} width={26} type="Oval" />} Carregando</span>
+        ) : enrollmentData !== '' ? (
+          <>
+            {paymentData !== '' && paymentData.status === 'PAID' ? (
+              <>
+                <h4>Ingresso escolhido</h4>
                 <Choices>
                   <ChoiceBox
-                    description={'Sem Hotel'}
-                    price={0}
-                    selectState={hotelSelector === '' ? false : hotelSelector}
-                    selector={setHotelSelector}
-                    setPrice={setHotelPrice}
-                  />
-                  <ChoiceBox
-                    description={'Com Hotel'}
-                    price={350}
-                    selectState={hotelSelector === '' ? false : !hotelSelector}
-                    selector={setHotelSelector}
-                    setPrice={setHotelPrice}
+                    description={
+                      (paymentData.TicketType.isRemote ? 'Remoto' : 'Presencial') +
+                      (paymentData.TicketType.includesHotel ? ' + Com Hotel' : ' + Sem Hotel')
+                    }
+                    price={Number(paymentData.TicketType.price)}
+                    selectState={true}
+                    disable={true}
                   />
                 </Choices>
-              </SecondStep>
-              <ThirdStep hide={(typeSelector === true && hotelSelector === '') || typeSelector === '' ? true : false}>
-                <h4>
-                  Fechado! O total ficou em <strong>{!typeSelector ? Number(typePrice) : Number(typePrice)+Number(hotelPrice)}</strong>. Agora é só confirmar:
-                </h4>
-                <ConfirmButton onClick={() => submitTicket()}>RESERVAR INGRESSO</ConfirmButton>
-              </ThirdStep>
-            </>}
-        </> : <span>
-          <h4>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</h4>
-        </span>
-        }
+                <h4>Pagamento</h4>
+                <>
+                  <h3>Pagamento confirmado!</h3>
+                  <h2>Prossiga para a escolha de hospedagem e atividades</h2>
+                </>
+              </>
+            ) : paymentData !== '' && paymentData.status === 'RESERVED' ? (
+              <>
+                <Choices>
+                  <ChoiceBox
+                    description={'Presencial' + ' + Com Hotel'}
+                    price={500}
+                    selectState={true}
+                    disable={true}
+                  />
+                </Choices>
+                <CreditCard ticketId={ticketId} />
+              </>
+            ) : (
+              <>
+                <h4>Primeiro, escolha sua modalidade de ingresso</h4>
+                <Choices>
+                  <ChoiceBox
+                    description={'Presencial'}
+                    price={250}
+                    selectState={typeSelector === '' ? false : typeSelector}
+                    selector={setTypeSelector}
+                    setPrice={setTypePrice}
+                  />
+                  <ChoiceBox
+                    description={'Remoto'}
+                    price={100}
+                    selectState={typeSelector === '' ? false : !typeSelector}
+                    selector={setTypeSelector}
+                    setPrice={setTypePrice}
+                  />
+                </Choices>
+                <SecondStep remote={!typeSelector}>
+                  <h4>Ótimo! Agora escolha sua modalidade de hospedagem</h4>
+                  <Choices>
+                    <ChoiceBox
+                      description={'Sem Hotel'}
+                      price={0}
+                      selectState={hotelSelector === '' ? false : hotelSelector}
+                      selector={setHotelSelector}
+                      setPrice={setHotelPrice}
+                    />
+                    <ChoiceBox
+                      description={'Com Hotel'}
+                      price={350}
+                      selectState={hotelSelector === '' ? false : !hotelSelector}
+                      selector={setHotelSelector}
+                      setPrice={setHotelPrice}
+                    />
+                  </Choices>
+                </SecondStep>
+                <ThirdStep hide={(typeSelector === true && hotelSelector === '') || typeSelector === '' ? true : false}>
+                  <h4>
+                    Fechado! O total ficou em{' '}
+                    <strong>{!typeSelector ? Number(typePrice) : Number(typePrice) + Number(hotelPrice)}</strong>. Agora
+                    é só confirmar:
+                  </h4>
+                  <ConfirmButton
+                    onClick={() => {setTicket({
+                      isRemote: !typeSelector,
+                      includesHotel: hotelSelector === '' || !typeSelector === true ? false : !hotelSelector,
+                      price: typePrice === 100 ? Number(typePrice) : Number(typePrice) + Number(hotelPrice),
+                    }); submitTicket();}}
+                  >
+                    RESERVAR INGRESSO
+                  </ConfirmButton>
+                </ThirdStep>
+              </>
+            )}
+          </>
+        ) : (
+          <span>
+            <h4>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</h4>
+          </span>
+        )}
       </Wrapper>
     );
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
 
 const Wrapper = styled.div`
@@ -149,13 +175,13 @@ const Wrapper = styled.div`
   h2 {
     margin-top: 6px;
     font-size: 16px;
-    color: #A9A9A9;
+    color: #a9a9a9;
   }
 
   h3 {
     font-size: 16px;
     font-weight: bold;
-    color: #4F4F4F;
+    color: #4f4f4f;
   }
 
   h4 {
@@ -204,11 +230,11 @@ const StyledLoader = styled(Loader)`
 `;
 
 const SecondStep = styled.div`
-  display: ${props => props.price === 0 || props.remote === true ? 'none' : 'flex'};
+  display: ${(props) => (props.price === 0 || props.remote === true ? 'none' : 'flex')};
   flex-direction: column;
 `;
 
 const ThirdStep = styled.div`
-  display: ${props => props.hide ? 'none' : 'flex'};
+  display: ${(props) => (props.hide ? 'none' : 'flex')};
   flex-direction: column;
 `;
