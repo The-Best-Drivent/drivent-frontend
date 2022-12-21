415f57d68916703716b4697080af5f3817014880
Alice Lipski <alipski@pop-os.localdomain>
2022-12-21 11:09:34 -0300

    feat: message screens in hotel tab when user cannot book hotel

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 import useHotel from '../../../hooks/api/useHotel';
 import useBooking from '../../../hooks/api/useBooking';
+import usePaymentPaid from '../../../hooks/api/usePayment';
 import Loader from 'react-loader-spinner';
 import HotelCard from './HotelCard';
 import RoomCard from './RoomCard';
@@ -15,33 +16,42 @@ export default function Hotel() {
   const token = useToken();
   const { hotels, hotelsLoading } = useHotel();
   const { bookings, bookingLoading } = useBooking();
-  const [ hotelsData, setHotelsData ] = useState([]);
-  const [ selectedHotel, setSelectedHotel] = useState({});
-  const [ selectedRoom, setSelectedRoom] = useState({});
-  const [ bookingData, setBookingData ] = useState({});
+  const [hotelsData, setHotelsData] = useState([]);
+  const [selectedHotel, setSelectedHotel] = useState({});
+  const [selectedRoom, setSelectedRoom] = useState({});
+  const [bookingData, setBookingData] = useState({});
+  const [paymentData, setPaymentData] = useState('');
+  const { paymentLoading, payment, enrollmentLoading, enrollment } = usePaymentPaid();
+
   const navigate = useNavigate();
 
   useEffect(() => {
     console.log(hotels);
-    if(hotels) {
+    if (hotels) {
       setHotelsData(hotels);
     }
   }, [hotels]);
 
   useEffect(() => {
-    if(bookings) {
+    if (bookings) {
       setBookingData(bookings);
     }
   }, [bookings]);
 
+  useEffect(() => {
+    if (payment) {
+      setPaymentData(payment);
+    }
+  }, [payment]);
+
   async function createBooking() {
     const body = { roomId: selectedRoom.id };
-    try{
+    try {
       const booking = await bookingApi.postBooking({ token, body });
       setBookingData(booking);
       toast('Quarto reservado com sucesso!');
       navigate('/dashboard/activities');
-    } catch(err) {
+    } catch (err) {
       console.log(err);
       toast('Não foi possível reservar o quarto!');
     }
@@ -49,56 +59,70 @@ export default function Hotel() {
 
   return (
     <Wrapper>
-      <StyledTypography variant='h4'>Escolha de hotel e quarto</StyledTypography>
-      {hotelsLoading || bookingLoading ? <span>
-        {<StyledLoader color="#000000" height={26} width={26} type="Oval" />} Carregando
-      </span> : bookingData.booking && hotelsData !== [] ? <>
-        
-        <StyledTypography variant='h6'>Você já escolheu seu quarto:</StyledTypography>
-      
-        <HotelsWrapper>
-          <HotelButton key={bookingData.booking.Room.Hotel.name} booking={true}>
-            <HotelImg src={bookingData.booking.Room.Hotel.image} alt={bookingData.booking.Room.Hotel.name}/>
-            <h1>{bookingData.booking.Room.Hotel.name}</h1>
+      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
+      {paymentData !== '' && paymentData.status === 'RESERVED' ? (
+        <NotDisplayable><StyledTypography variant="h6">
+          Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem
+        </StyledTypography></NotDisplayable>
+      ) : paymentData !== '' && paymentData.TicketType.includesHotel === false ? (
+        <NotDisplayable><StyledTypography variant="h6">
+          Sua modalidade de ingresso não inclui hospedagem. Prossiga para a escolha de atividades
+        </StyledTypography></NotDisplayable>
+      ) : hotelsLoading || bookingLoading ? (
+        <span>{<StyledLoader color="#000000" height={26} width={26} type="Oval" />} Carregando</span>
+      ) : bookingData.booking && hotelsData !== [] ? (
+        <>
+          <StyledTypography variant="h6">Você já escolheu seu quarto:</StyledTypography>
 
-            <h4>{'Quarto Reservado'}</h4>
-            <h2>{bookingData.booking.Room.name + (bookingData.booking.Room.capacity === 1 ? ' (Single)' : ' (Double)')}</h2>
+          <HotelsWrapper>
+            <HotelButton key={bookingData.booking.Room.Hotel.name} booking={true}>
+              <HotelImg src={bookingData.booking.Room.Hotel.image} alt={bookingData.booking.Room.Hotel.name} />
+              <h1>{bookingData.booking.Room.Hotel.name}</h1>
 
-            <h4>{'Pessoas no seu quarto'}</h4>
-            <h2>{bookingData._count === 1 ? 'Somente você' : 'Você e mais ' + bookingData._count}</h2>
-          </HotelButton>
-        </HotelsWrapper>
-        <ConfirmButton onClick={() => console.log('aqui')}>TROCAR DE QUARTO</ConfirmButton>
-      </> : <>
-        <StyledTypography variant='h6'>Primeiro, escolha seu hotel</StyledTypography>
-        
-        <HotelsWrapper>
-          {hotelsData.map( (hotel) => {
-            return (
-              <HotelCard setSelectedHotel={setSelectedHotel} selectedId={selectedHotel.id} hotel={hotel} key={hotel.id} />
-            );
-          })}
-        </HotelsWrapper>
-        {
-          selectedHotel.id?
+              <h4>{'Quarto Reservado'}</h4>
+              <h2>
+                {bookingData.booking.Room.name + (bookingData.booking.Room.capacity === 1 ? ' (Single)' : ' (Double)')}
+              </h2>
+
+              <h4>{'Pessoas no seu quarto'}</h4>
+              <h2>{bookingData._count === 1 ? 'Somente você' : 'Você e mais ' + bookingData._count}</h2>
+            </HotelButton>
+          </HotelsWrapper>
+          <ConfirmButton onClick={() => console.log('aqui')}>TROCAR DE QUARTO</ConfirmButton>
+        </>
+      ) : (
+        <>
+          <StyledTypography variant="h6">Primeiro, escolha seu hotel</StyledTypography>
+
+          <HotelsWrapper>
+            {hotelsData.map((hotel) => {
+              return (
+                <HotelCard
+                  setSelectedHotel={setSelectedHotel}
+                  selectedId={selectedHotel.id}
+                  hotel={hotel}
+                  key={hotel.id}
+                />
+              );
+            })}
+          </HotelsWrapper>
+          {selectedHotel.id ? (
             <>
-              <StyledTypography variant='h6'>Ótima pedida! Agora escolha seu quarto:</StyledTypography>
+              <StyledTypography variant="h6">Ótima pedida! Agora escolha seu quarto:</StyledTypography>
               <RoomsWrapper>
                 {selectedHotel.Rooms.map((room) => {
-                  return(<RoomCard room={room} selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} key={room.id}/>);
+                  return (
+                    <RoomCard room={room} selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} key={room.id} />
+                  );
                 })}
               </RoomsWrapper>
             </>
-            :
+          ) : (
             <></>
-        }
-        {
-          selectedRoom.id?
-            <ConfirmButton onClick={() => createBooking()}>RESERVAR QUARTO</ConfirmButton>
-            :
-            <></>
-        }
-      </>}
+          )}
+          {selectedRoom.id ? <ConfirmButton onClick={() => createBooking()}>RESERVAR QUARTO</ConfirmButton> : <></>}
+        </>
+      )}
     </Wrapper>
   );
 }
@@ -106,8 +130,10 @@ export default function Hotel() {
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  h6{
-    color: #8E8E8E;
+  height: 100%;
+  width: 100%;
+  h6 {
+    color: #8e8e8e;
     font-weight: 400;
   }
 `;
@@ -115,29 +141,27 @@ const Wrapper = styled.div`
 const RoomsWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  &>*{
-    margin-right:20px;
+  & > * {
+    margin-right: 20px;
   }
 `;
 
 const HotelsWrapper = styled.div`
   display: flex;
   font-family: 'Roboto';
-  margin-bottom:10px;
+  margin-bottom: 10px;
 `;
 
 const HotelButton = styled.div`
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
-  padding:10px;
-  background-color: ${props => props.booking ? '#FFEED2' : '#EBEBEB'};
-  width:200px;
-  height:300px;
-  margin-right:20px;
+  padding: 10px;
+  background-color: ${(props) => (props.booking ? '#FFEED2' : '#EBEBEB')};
+  width: 200px;
+  height: 300px;
+  margin-right: 20px;
   border-radius: 12px;
-
-  
 
   h3 {
     font-size: 18px;
@@ -163,9 +187,9 @@ const HotelButton = styled.div`
     text-align: center;
     margin-right: 8px;
   }
-  
+
   h4,
-  h1{
+  h1 {
     width: 100%;
     font-size: 20px;
   }
@@ -179,27 +203,37 @@ const HotelButton = styled.div`
   h2 {
     margin: 6px 0;
   }
-  
-  &:hover{
+
+  &:hover {
     cursor: pointer;
     opacity: 0.9;
   }
-  
-  &:active{
+
+  &:active {
     transform: translateY(4px);
   }
 `;
 
 const HotelImg = styled.img`
-  width:100%;
-  height:100px;
+  width: 100%;
+  height: 100px;
   object-fit: cover;
   border-radius: 12px;
-  margin-bottom:10px;
+  margin-bottom: 10px;
 `;
 
 const StyledTypography = styled(Typography)`
-  margin-bottom: 20px!important;
+  margin-bottom: 20px !important;
+`;
+
+const NotDisplayable = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 0 150px;
+  text-align: center;
 `;
 
 const StyledLoader = styled(Loader)`
@@ -221,12 +255,12 @@ const ConfirmButton = styled.button`
   background: #e0e0e0;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   border-radius: 4px;
-  &:hover{
+  &:hover {
     cursor: pointer;
     opacity: 0.9;
   }
-  
-  &:active{
+
+  &:active {
     transform: translateY(4px);
   }
 `;
